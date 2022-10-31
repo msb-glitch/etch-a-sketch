bde7a367140b0155d058e40214357516ded9f995
M. Scott Brauer <msb@mscottbrauer.com>
2022-10-31 09:27:49 -0400

    Start setting up 16x16 grid.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello world");
+const grid = document.querySelector('#grid');
+const gridBox = '<div class="gridbox">&nbsp;</div>';
+
+let gridDimension = 16;
+
+for (let columnCount = 0; columnCount < gridDimension; columnCount++) {
+    for (let rowCount = 0; rowCount < gridDimension; rowCount++) {
+        grid.innerHTML += gridBox;
+    }
+}

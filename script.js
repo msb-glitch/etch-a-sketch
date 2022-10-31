5fdd0fcc0420a7e0a7c5738e5d613a09a4fd5818
M. Scott Brauer <msb@mscottbrauer.com>
2022-10-31 09:29:18 -0400

    Add pseudocode for reminder of what to do to set up grid.

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const gridBox = '<div class="gridbox">&nbsp;</div>';
 
 let gridDimension = 16;
 
+// set up grid box size
+// add correct number of boxes to grid in row and column
+
 for (let columnCount = 0; columnCount < gridDimension; columnCount++) {
     for (let rowCount = 0; rowCount < gridDimension; rowCount++) {
         grid.innerHTML += gridBox;

46cc060c50c3e8820122f8cfad8bd7f399d7c6ee
M. Scott Brauer <msb@mscottbrauer.com>
2022-11-01 09:30:11 -0400

    Build grid (still has errors); fill test area with data

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
 const grid = document.querySelector('.grid');
-const gridBox = '<div class="gridbox"></div>';
+const gridBox = document.createElement('div')
+gridBox.className = 'gridbox';
 
 let gridDimension = 16;
+let gridWidth = grid.offsetWidth;
+let gridHeight = grid.offsetHeight;
 
 // set up grid box size
 // add correct number of boxes to grid in row and column
 
 for (let columnCount = 0; columnCount < gridDimension; columnCount++) {
     for (let rowCount = 0; rowCount < gridDimension; rowCount++) {
-        grid.innerHTML += gridBox;
+        grid.appendChild(createGridBox(gridWidth / gridDimension));
     }
 }
+
+function createGridBox(size){
+    const gridBox = document.createElement('div')
+    gridBox.className = 'gridbox';
+    gridBox.style.width= `${size}px`;
+    gridBox.style.height= `${size}px`;
+
+    return gridBox;
+}
+
+
+
+
+
+
+
+
+//test info
+
+document.querySelector('.gridwidth').textContent = gridWidth;
+
+document.querySelector('.gridheight').textContent = gridHeight;
+
+let gridBoxWidth = document.querySelector('.gridbox').offsetWidth;
+document.querySelector('.gridboxwidth').textContent = gridBoxWidth;
+let gridBoxHeight = document.querySelector('.gridbox').offsetHeight;
+document.querySelector('.gridboxheight').textContent = gridBoxHeight;

381a6b502c7b3ea960c8cf7f3e40e348808c603a
M. Scott Brauer <msb@mscottbrauer.com>
2022-11-30 16:25:57 -0500

    Redo gridBox; Add eventlistener for hover to change color on gridboxes.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const grid = document.querySelector('.grid');
-const gridBox = document.createElement('div')
-gridBox.className = 'gridbox';
 
-let gridDimension = 12;
+
+
+let gridDimension = 18; // doesn't display right at numbers lower than ~8
 let gridWidth = grid.offsetWidth;
 let gridHeight = grid.offsetHeight;
 
@@ -11,23 +11,26 @@ let gridHeight = grid.offsetHeight;
 
 for (let columnCount = 0; columnCount < gridDimension; columnCount++) {
     for (let rowCount = 0; rowCount < gridDimension; rowCount++) {
-        grid.appendChild(createGridBox((gridWidth)/ (gridDimension +1))); // figure out why + 1 makes the right grid size
+        grid.appendChild(createGridBox((gridWidth) / (gridDimension + 1))); // figure out why + 1 makes the right grid size
     }
 }
 
-function createGridBox(size){
-    const gridBox = document.createElement('div')
-    gridBox.className = 'gridbox';
-    gridBox.style.width= `${size}px`;
-    gridBox.style.height= `${size}px`;
+function createGridBox(size) {
+    const newBox = document.createElement('div');
+    newBox.className = 'gridbox';
+    newBox.style.width = `${size}px`;
+    newBox.style.height = `${size}px`;
 
-    return gridBox;
+    return newBox;
 }
 
+const gridBox = document.querySelectorAll('.gridbox'); // get all boxes in the grid
 
-
-
-
+gridBox.forEach(box => {
+    box.addEventListener('mouseover', () =>{
+        box.classList.add('filledin');
+    })
+})
 
 
 

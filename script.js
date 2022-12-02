27caabe8caca1fb8d94b6f4231c713ca9dbb25d5
M. Scott Brauer <msb@mscottbrauer.com>
2022-12-02 12:11:08 -0500

    Fix createGridBox with css grid

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 const grid = document.querySelector('.grid');
-
+let gridBox = [];
 let bgColor = '#00b4d8';
 const bodyBackgroundColor = window.getComputedStyle(document.body, null).getPropertyValue('background-color');
-let gridDimension = 18; // doesn't display right at 2 or lower; 19 or higher
+let gridDimension = 20; // doesn't display right at 2 or lower; 19 or higher
 let gridWidth = grid.offsetWidth;
 let gridHeight = grid.offsetHeight;
 
 // set up grid box size
 // add correct number of boxes to grid in row and column
 
-for (let columnCount = 0; columnCount < gridDimension; columnCount++) {
-    for (let rowCount = 0; rowCount < gridDimension; rowCount++) {
-        grid.appendChild(createGridBox((gridWidth) / (gridDimension), rowCount + 1));
-
-    }
-}
+createGridBox(gridDimension)
 
 function createGridBox(size, number) { //add number to have box count in box
-    const newBox = document.createElement('div');
-    newBox.className = 'gridbox';
-    newBox.style.width = `${size - 2}px`;
-    newBox.style.height = `${size - 2}px`;
-    if (number) {
-        newBox.textContent = number;
+    
+    grid.style.gridTemplateColumns = (`repeat(${size}, 1fr`);
+    grid.style.gridTemplateRows = (`repeat(${size}, 1fr`);
+    for (let i = 0; i < size * size; i++){
+        gridBox[i] = document.createElement('div')
+        gridBox[i].classList.add('gridbox');
+        gridBox[i].textContent = i;
+        grid.appendChild(gridBox[i]);
+        
     }
-    return newBox;
+    
 }
 
-const gridBox = document.querySelectorAll('.gridbox'); // get all boxes in the grid
+//const gridBox = document.querySelectorAll('.gridbox'); // get all boxes in the grid
 
 //things to add
 //  click mode
@@ -77,7 +75,7 @@ document.querySelector('.gridwidth').textContent = gridWidth;
 
 document.querySelector('.gridheight').textContent = gridHeight;
 
-let gridBoxWidth = document.querySelector('.gridbox').offsetWidth;
+/*let gridBoxWidth = document.querySelector('.gridbox').offsetWidth;
 document.querySelector('.gridboxwidth').textContent = gridBoxWidth;
 let gridBoxHeight = document.querySelector('.gridbox').offsetHeight;
-document.querySelector('.gridboxheight').textContent = gridBoxHeight;
+document.querySelector('.gridboxheight').textContent = gridBoxHeight;*/
